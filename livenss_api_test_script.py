be619c62ca897e9cacc5e367d903067d24d50c07
Nivratti <boyanenivratti@gmail.com>
2022-04-16 19:04:47 +0530

    added feature to save json response

--- livenss_api_test_script.py
+++ livenss_api_test_script.py
@@ -6,44 +6,65 @@ from nb_utils.file_dir_handling import list_files ## pip install nb_utils
 import argparse 
 import glob
 import os 
+from pathlib import Path
 
-parser = argparse.ArgumentParser("Argument for passig folder")
-parser.add_argument("--dir_path",type=str,help="pass image folder name to test images",default="/home/azhar/liveness_test/Liveness-Tester/sample")
-args = parser.parse_args()
+def call_Api(image, api_url) :
+    # url = "https://live.accurascan.com/upload.php"
+    # api_url = "http://164.52.218.61/upload.php"
 
-new_path = args.dir_path 
-ext_list = ["*.jpg","*.png","*.jpeg"]
-
-image_data = [] 
-spoof_result = [] 
-real_result  = []
-raw_result = [] 
-
-image_data = list_files(new_path, filter_ext=[".png", ".jpg", ".jpeg"])
-
-def call_Api(image) :
-    url = "https://live.accurascan.com/upload.php"
     payload={}
     files=[
         # ('photo',('file',open('/home/azhar/liveness_test/11.png','rb'),'image/png')),
       ('photo',(os.path.basename(image),open(image,'rb'),'image/jpg'))
     ]
     headers = {}
-    response = requests.request("POST", url, headers=headers, data=payload, files=files, verify=False)
+    response = requests.request("POST", api_url, headers=headers, data=payload, files=files, verify=False)
     return json.loads(response.text)
 
-for image in image_data :
-    api_response  = call_Api(image)
-    print(api_response)
-    with open("response.json",'a') as f :
-        f.write(f"{json.dumps(api_response, sort_keys=True, indent=4)},")
-        
-    # if api_response["type"] == "success"  and api_response["label"] == "spoof" :
-    #     spoof_result.append(api_response)
-        
-    # elif api_response["type"] == "success"  and api_response["label"] == "real" :
-    #     real_result.append(api_response)
+def main():
+    parser = argparse.ArgumentParser("Argument for passig folder")
+    parser.add_argument("--dir_path",type=str,help="pass image folder name to test images",default="/home/azhar/liveness_test/Liveness-Tester/sample")
     
-    # else :
-    #     raw_result.append(api_response)
+    parser.add_argument("--json_result_base_dir",type=str, help="folder name to store json respose",default="json_response")
+
+    parser.add_argument("--api_url",type=str, default="http://164.52.218.61/upload.php", help="API Url to check liveness")
+    
+    args = parser.parse_args()
+
+    print(f"Using api Url: ", args.api_url)
+
+    dir_path = args.dir_path 
+    json_result_base_dir = args.json_result_base_dir
+
+    lst_image_files = list_files(
+        dir_path, filter_ext=[".png", ".jpg", ".jpeg"], 
+        return_relative_path=True
+    )
+
+    for image_rel_filepath in lst_image_files :
+        # print(f"image_rel_filepath: ", image_rel_filepath)
+
+        image_abs_path = os.path.join(dir_path, image_rel_filepath) ## abasolute path
+        api_response  = call_Api(image_abs_path, args.api_url)
+        # print(f"api_response:", api_response)
+
+        json_out_file_relpath = Path(image_rel_filepath).with_suffix(".json")
+        json_outfile_abspath = os.path.join(
+            dir_path, json_result_base_dir, json_out_file_relpath
+        )
+        os.makedirs(os.path.dirname(json_outfile_abspath), exist_ok=True)
+
+        with open(json_outfile_abspath,'w') as f :
+            f.write(json.dumps(api_response, sort_keys=True, indent=4))
+            
+        # if api_response["type"] == "success"  and api_response["label"] == "spoof" :
+        #     spoof_result.append(api_response)
+            
+        # elif api_response["type"] == "success"  and api_response["label"] == "real" :
+        #     real_result.append(api_response)
         
+        # else :
+        #     raw_result.append(api_response)
+            
+if __name__ == "__main__":
+    main()
