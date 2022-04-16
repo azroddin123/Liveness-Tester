e296cce841509a3c42461df32caab34df34e5cdc
Nivratti <boyanenivratti@gmail.com>
2022-04-16 19:17:29 +0530

    added multiprocessign

--- livenss_api_test_script.py
+++ livenss_api_test_script.py
@@ -8,26 +8,63 @@ import glob
 import os 
 from pathlib import Path
 
-def call_Api(image, api_url) :
+from psutil import cpu_count
+from tqdm.auto import tqdm
+from tqdm.contrib.concurrent import process_map, thread_map  # requires tqdm>=4.42.0
+from functools import partial
+
+def call_Api(image_rel_filepath, dir_path, api_url, json_result_base_dir) :
     # url = "https://live.accurascan.com/upload.php"
     # api_url = "http://164.52.218.61/upload.php"
+
+    image_abs_path = os.path.join(dir_path, image_rel_filepath) ## abasolute path
 
     payload={}
     files=[
         # ('photo',('file',open('/home/azhar/liveness_test/11.png','rb'),'image/png')),
-      ('photo',(os.path.basename(image),open(image,'rb'),'image/jpg'))
+      ('photo',(os.path.basename(image_abs_path),open(image_abs_path,'rb'),'image/jpg'))
     ]
     headers = {}
     response = requests.request("POST", api_url, headers=headers, data=payload, files=files, verify=False)
-    return json.loads(response.text)
+    response_text = json.loads(response.text)
+
+    ###
+    json_out_file_relpath = Path(image_rel_filepath).with_suffix(".json")
+    json_outfile_abspath = os.path.join(
+        dir_path, json_result_base_dir, json_out_file_relpath
+    )
+    os.makedirs(os.path.dirname(json_outfile_abspath), exist_ok=True)
+
+    with open(json_outfile_abspath,'w') as f :
+        f.write(json.dumps(response_text, sort_keys=True, indent=4))
+        
+    # if api_response["type"] == "success"  and api_response["label"] == "spoof" :
+    #     spoof_result.append(api_response)
+        
+    # elif api_response["type"] == "success"  and api_response["label"] == "real" :
+    #     real_result.append(api_response)
+    
+    # else :
+    #     raw_result.append(api_response)
 
 def main():
     parser = argparse.ArgumentParser("Argument for passig folder")
-    parser.add_argument("--dir_path",type=str,help="pass image folder name to test images",default="/home/azhar/liveness_test/Liveness-Tester/sample")
-    
-    parser.add_argument("--json_result_base_dir",type=str, help="folder name to store json respose",default="json_response")
-
-    parser.add_argument("--api_url",type=str, default="http://164.52.218.61/upload.php", help="API Url to check liveness")
+    parser.add_argument(
+        "--dir_path", type=str,
+        help="pass image folder name to test images",
+        default="/home/azhar/liveness_test/Liveness-Tester/sample"
+    )
+    parser.add_argument(
+        "--json_result_base_dir", type=str, default="json_response",
+        help="folder name to store json respose"
+    )
+    parser.add_argument(
+        "--api_url", type=str, default="http://164.52.218.61/upload.php", 
+        help="API Url to check liveness"
+    )
+    parser.add_argument(
+        "-w", "--max_workers", type=int, required=False, help="", default=1
+    )
     
     args = parser.parse_args()
 
@@ -41,30 +78,30 @@ def main():
         return_relative_path=True
     )
 
-    for image_rel_filepath in lst_image_files :
-        # print(f"image_rel_filepath: ", image_rel_filepath)
+    if args.max_workers <= 1:
+        for image_rel_filepath in tqdm(lst_image_files) :
+            # print(f"image_rel_filepath: ", image_rel_filepath)
 
-        image_abs_path = os.path.join(dir_path, image_rel_filepath) ## abasolute path
-        api_response  = call_Api(image_abs_path, args.api_url)
-        # print(f"api_response:", api_response)
+            api_response  = call_Api(
+                image_rel_filepath, dir_path, args.api_url, json_result_base_dir
+            )
+            # print(f"api_response:", api_response)
+    else:
+        ## multiprocessing
+        worker = call_Api  # function to map
+        kwargs = {
+            'dir_path': dir_path,
+            'api_url': args.api_url,
+            'json_result_base_dir': json_result_base_dir
+        }
+        jobs = lst_image_files  # file_rel_paths
 
-        json_out_file_relpath = Path(image_rel_filepath).with_suffix(".json")
-        json_outfile_abspath = os.path.join(
-            dir_path, json_result_base_dir, json_out_file_relpath
+        result = process_map(
+            partial(worker, **kwargs), jobs, 
+            max_workers=args.max_workers
         )
-        os.makedirs(os.path.dirname(json_outfile_abspath), exist_ok=True)
-
-        with open(json_outfile_abspath,'w') as f :
-            f.write(json.dumps(api_response, sort_keys=True, indent=4))
-            
-        # if api_response["type"] == "success"  and api_response["label"] == "spoof" :
-        #     spoof_result.append(api_response)
-            
-        # elif api_response["type"] == "success"  and api_response["label"] == "real" :
-        #     real_result.append(api_response)
+        return result
+        pass
         
-        # else :
-        #     raw_result.append(api_response)
-            
 if __name__ == "__main__":
     main()
